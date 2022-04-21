b1bb6699d8566cb6fc0b80964cde9a7781301fb8
WebDeveloperGuide <support@webdeveloperguide.in>
2022-04-21 15:44:51 +0530

    Shopping Cart > Checkout > Close car slide when redirect to login page.

--- web_panel/src/components/CartTotal.js
+++ web_panel/src/components/CartTotal.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {showCart} from '../redux/actions/cartActions';
 
 const CartTotal = () => {
 	const cart = useSelector((state) => state.cart);
   	const { cartItems } = cart;
 	const cartTotal = cartItems.reduce((a, i) => a + i.qty * i.price, 0).toFixed(2);
+
+	const dispatch = useDispatch();
+	const closeCart = () => {
+		dispatch(showCart(false))
+	}
 	return(
 			<>
 				<footer>
 		            <h3 className="cart-total text-slanted">total : ${cartTotal}</h3>
 		            {
 		            	cartItems.length > 0 ? 
-		            	<Link to="/shipping"><button className="cart-checkout btn">checkout</button></Link>
+		            	<Link to="/shipping" onClick={closeCart}><button className="cart-checkout btn">checkout</button></Link>
 		            	:
 		            	""
 		            }
